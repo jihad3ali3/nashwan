4dfb9600bb7abd40f247b944bba8ff5f36f23341
nashwan <nashwany91@gmail.com>
2026-08-12 04:23:32 +0300

    Fix pino http TypeScript error

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,30 +1,30 @@
-import express, { type Express } from "express";
-import cors from "cors";
-import pinoHttp from "pino-http";
+import express, { type Express } from "express";import cors from "cors";
+import pinoHttp, { type Options } from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
 const app: Express = express();
 
-app.use(
-  pinoHttp({
-    logger,
-    serializers: {
-      req(req) {
-        return {
-          id: req.id,
-          method: req.method,
-          url: req.url?.split("?")[0],
-        };
-      },
-      res(res) {
-        return {
-          statusCode: res.statusCode,
-        };
-      },
+const pinoOptions: Options = {
+  logger,
+  serializers: {
+    req(req) {
+      return {
+        id: req.id,
+        method: req.method,
+        url: req.url?.split("?")[0],
+      };
     },
-  }),
-);
+    res(res) {
+      return {
+        statusCode: res.statusCode,
+      };
+    },
+  },
+};
+
+app.use(pinoHttp(pinoOptions));
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -32,3 +32,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api", router);
 
 export default app;
+
+
+
+
+
